de375e8fcf6594d341560dcf6eb741f383c48ec5
Varun Patil <varun.patil578@gmail.com>
2024-05-02 12:11:33 +0530

    migrated to latest version of SDK

---
+++ services/aps.js
@@ -0,0 +1,15 @@
+const { SdkManagerBuilder } = require('@aps_sdk/autodesk-sdkmanager');
+const { AuthenticationClient, Scopes } = require('@aps_sdk/authentication');
+const { APS_CLIENT_ID, APS_CLIENT_SECRET } = process.env;
+
+const sdk = SdkManagerBuilder.create().build();
+const authenticationClient = new AuthenticationClient(sdk);
+
+const service = module.exports = {};
+
+service.getPublicToken = async () => {
+    const credentials = await authenticationClient.getTwoLeggedToken(APS_CLIENT_ID, APS_CLIENT_SECRET, [
+        Scopes.DataRead
+    ]);
+    return credentials;
+};
